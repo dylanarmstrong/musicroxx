d4dc6d3b481a8ea132bd750ad61b3a049aed4bd5
Dylan Armstrong <dylanarmstrong116@gmail.com>
2012-04-19 14:59:21 -0500

    Removed icon border so they looked normal

---
+++ wind.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'wind.ui'
+#
+# Created: Thu Apr 19 14:58:12 2012
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
