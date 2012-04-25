64c8d8b55093ad10670cc3892a07c28f3c54e1b3
Dylan Armstrong <dylanarmstrong116@gmail.com>
2012-04-25 11:49:02 -0500

    I added a setup.py file, but honestly I have no idea what its for..

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(
+    name='musicroxx',
+    version='0.3',
+    description='mpd client written in python using PyQt',
+    author='Dylan Armstrong',
+    author_email='dylanarmstrong116@gmail.com',
+    url='http://github.com/meinhimmel/musicroxx',
+    keywords='mpd musicroxx',
+    packages=[],
+)
